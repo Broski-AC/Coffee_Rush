7354b823d8ef7ed9b477dbf3ef8b0154a0f0cf2b
Allison Broski <41844551+Broski-AC@users.noreply.github.com>
2018-10-06 19:33:34 -0400

    Add files via upload

---
+++ CircleMechanics.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleMechanics : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        transform.localScale += new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            while (transform.localScale.y > 0.5)
+            {
+                transform.localScale += new Vector3(0, -0.5f, 0);
+            }
+        }
+        else 
+        {
+            while (transform.localScale.y <= 0.5)
+            {
+                transform.localScale += new Vector3(0, 0.5f, 0);
+            }
+        }
+    }
+}
+
+
---
+++ New Test Script.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewTestScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	// Everything here will run immediately	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	// This will check for updates on every frame.	
+	}
+}
